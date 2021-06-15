9e3548216b5817192e113f5123cf0d6741520f11
kishan <kishanvaghani40@gmail.com>
2021-06-15 12:29:06 +0530

    third commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,48 +5,55 @@ import { ServicesService } from '../services.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  constructor(private fb: FormBuilder, private auth: ServicesService, private router: Router) { }
+  error:boolean=false;
+  constructor(
+    private fb: FormBuilder,
+    private auth: ServicesService,
+    private router: Router
+  ) {}
   ngOnInit() {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required]],
       pass: ['', [Validators.required]],
-
     });
   }
   login() {
     if (this.loginForm.value) {
       console.log(this.loginForm.value);
-
       let mailData = new FormData();
-      mailData.append("email", this.loginForm.controls.email.value);
-      mailData.append("password", this.loginForm.controls.pass.value);
-      mailData.append("invitation_code", "");
-      mailData.append("dev_id", "");
-      mailData.append("rememberMe", "false");
+      mailData.append('email', this.loginForm.controls.email.value);
+      mailData.append('password', this.loginForm.controls.pass.value);
+      mailData.append('invitation_code', '');
+      mailData.append('dev_id', '');
+      mailData.append('rememberMe', 'false');
       console.log(mailData);
 
-      this.auth.loginUser(mailData).subscribe((response) => {
-        console.log(response);
-        if (response.token) {
-          localStorage.setItem("token", response.token);
-          this.router.navigate(['logic']);
-        }else{
-          alert("entre valid email or pass")
-
-        }
-      },
+      this.auth.loginUser(mailData).subscribe(
+        (response) => {
+          console.log(response);
+          if (response.token) {
+            console.log(response);
+            localStorage.setItem('token', response.token);
+            this.router.navigate(['logic']);
+          } else {
+            
+            alert('entre valid email or pass');
+          }
+        },
         (error) => {
           // this.spinner.hide();
-          alert("User name or password is incorrect")
-        });
+          this.error=true;
+          console.log(error);
+          alert('User name or password is incorrect');
+        }
+      );
     } else {
-      alert("please entre all value")
+      alert('please entre all value');
     }
-
   }
-
+  get err() { return this.loginForm.controls }
 }
